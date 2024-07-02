bc830cac2dbdfc00117a021f1e401db8096c8b20
Elvis Makia <elvis_makia@yahoo.com>
2024-07-02 19:28:21 -0400

    3nd  commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,51 @@
-import React from 'react';
+
 import ReactDOM from 'react-dom/client';
 
+function MainHeader(){
+    return(<h1>REACT COURSE</h1>)
+}
+
+function SubHeader(){
+    return(<p>This is an exciting course</p>)
+}
+
+
+function Header(){
+    return(
+        <div>
+    <MainHeader></MainHeader>
+    <SubHeader></SubHeader>
+       </div>
+    )
+}
+
+function MainBody(){
+    return(
+        <div>
+            <p>In this course, we will learn react js by building TAskOpedia</p>
+            <ul>
+                <li>Call Ben</li>
+                <li>Go to walmart</li>
+            </ul>
+        </div>
+    );
+}
+
+function Footer(){
+    return(<p>Happy Coding!</p>)
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<div>Hello World</div>
+root.render(
+<div>
+    
+    Hello World
+    <Header/>
+    <MainBody/>
+    <Footer/>
+
+</div>
+
+
 );
 
